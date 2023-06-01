81436ef9c1f5ba876c045a0bb1289b5d314a8317
tristan <tristan.wermelinger@gmail.com>
2023-06-01 11:54:17 +0200

    commit bugs route

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 const sapins = require("./sapins.json");
-console.log(sapins);
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
+  //console.log(sapins);
+  const arr = req.query.arrdt;
+  //console.log(arr);
   let results = [];
   for (let i = 0; i < sapins.length; i++) {
-    let sapins = {
-      garden: sapins[i].garden,
-      address: address[i].address,
-      arrdt: arrdt[i].arrdt,
+    let infos = {
+      garden: sapins[i].fields.jardin,
+      address: sapins[i].fields.adresse,
+      arrdt: sapins[i].fields.arrdt,
     };
-    results.push(sapins);
+    //console.log(sapins[i].garden);
+    results.push(infos);
   }
   res.json(results);
 });
